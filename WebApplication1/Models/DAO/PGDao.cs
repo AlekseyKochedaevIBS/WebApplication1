b394edf429e785b353f00d9be009f1869692b57a
aleksey_kochedaev@epam.com <aleksey_kochedaev@epam.com>
2022-08-31 11:42:49 +0400

    Add project files.

---
+++ WebApplication1/Models/DAO/PGDao.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+
+namespace WebApplication1.Models.DAO
+{
+	public class PGDao
+	{
+		public object? GetValue()
+		{
+			var connString = "Host=localhost;Username=postgres;Password=;Database=test";
+
+			using var conn = new NpgsqlConnection(connString);
+			conn.Open();
+
+			// Insert some data
+			//using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+			//{
+			//	cmd.Parameters.AddWithValue("p", "Hello world");
+			//	cmd.ExecuteNonQueryAsync();
+			//}
+
+			// Retrieve all rows
+			using (var cmd = new NpgsqlCommand("SELECT \"Id\", text	FROM public.course", conn))
+			using (var reader = cmd.ExecuteReader())
+			{
+				if (reader.Read())
+				{
+					return reader.GetValue(0);
+				}
+			}
+			return null;
+		}
+	}
+}
